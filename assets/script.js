23d66296399d27dfa7c6eb8e88cd7cb1f009ddaa
noah138 <noahschwartz@live.com>
2022-10-11 23:31:29 -0700

    history is functional, but 5-day forecast does not run

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,47 @@
 let apiKey = '9360ff9c9f6d95d042d8df33057b529a'
 
-var forecast = document.querySelector('.forecast');
-// var inputValue = document.querySelector('.input-search');
-// var name = document.querySelector('city-name');
-// var temp = document.querySelector('city-temp');
-// var wind = document.querySelector('city-wind');
-// var humidity = document.querySelector('city-humidity');
+const forecast = $('.forecast')
+const searchHistory = $('#history')
 
-// button.addEventListener('click', function() {
-//     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=9360ff9c9f6d95d042d8df33057b529a')
-//         .then(response => response.json())
-//         .then(data => console.log(data))
+var cities = JSON.parse(localStorage.getItem('cities')) || [];
 
-//     .catch(err => alert("Wrong city name"))
-// })
+$(document).ready(() => {
+    cities.forEach((city) => {
+        searchHistory.prepend(
+            $(`<li class="list-group-item text-capitalize">${city}</li>`)
+        )
+    })
+
+    $('.list-group-item').on('click', (e) => {
+        const city = e.target.textContent;
+        weather.fetchWeather(city);
+        weather.displayWeather(city);
+        futureWeather.fetchFutureWeather(city);
+        futureWeather.displayFutureWeather(city);
+    })
+})
+
+$('.btn').on("click", (e) => {
+    event.preventDefault();
+    
+    var city = $('input').val();
+    cities.push(city);
+
+    localStorage.setItem('cities', JSON.stringify(cities));
+
+    searchHistory.prepend($(`<li class="list-group-item">${city}</li>`));
+
+    $('.list-group-item').on('click', (e) => {
+        const city = e.target.textContent;
+        weather.fetchWeather(city);
+        weather.displayWeather(city);
+        futureWeather.fetchFutureWeather(city);
+        futureWeather.displayFutureWeather(city);
+    })
+
+    weather.search();
+    futureWeather.search();
+})
 
 let weather = {
     fetchWeather: function(city) {
@@ -24,23 +52,19 @@ let weather = {
         .then((data) => this.displayWeather(data));
     },
     displayWeather: function(data) {
-        var {timezone} = data;
+        var {dt} = data;
         var {name} = data;
-        var {temp, humidity} = data.main;
+        const {temp, humidity} = data.main;
         var {speed} = data.wind;
         
-        var date = new Date(timezone * 1000);
-        var month = date.getMonth();
-        var day = date.getDate();
-        var year = date.getFullYear();
-        var formattedDate = (month+"/"+day+"/"+year);
+        const dateObject = new Date(dt*1000);
+        const formattedDate = dateObject.toLocaleDateString();
 
         document.querySelector('#city-name').innerText = name + " (" + formattedDate + ")"
-        document.querySelector('#icon').src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+        document.querySelector('#icon').src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
         document.querySelector('#city-temp').innerText = "Temp: " + temp + " °F"
         document.querySelector('#city-wind').innerText = "Wind: " + speed + " MPH"
         document.querySelector('#city-humidity').innerText = "Humidity " + humidity + " %"
-
     },
     search: function() {
         this.fetchWeather(document.querySelector(".input-search").value);
@@ -57,50 +81,32 @@ let futureWeather = {
         
     },
     displayFutureWeather: function(data) {
-        for (i=4; i < data.list.length; i+=8) {
+        for (let i=4; i < 37; i+=8) {
+
             var {dt} = data.list[i];
             var {temp, humidity} = data.list[i].main;
-            var {speed} = data.list[i].wind
-            const {id} = data.list[i].weather
+            var {speed} = data.list[i].wind;
 
-            const milliseconds = dt*1000
-            const dateObject = new Date(milliseconds)
-            const humanDateFormat = dateObject.toLocaleString()
-            const formattedDate = dateObject.toLocaleString("en-US", {timeZoneName: "short"})
+            const dateObject = new Date(dt*1000);
+            const formattedDate = dateObject.toLocaleDateString();
             
-            console.log(formattedDate, temp, humidity, speed, id)
+            console.log(formattedDate, temp, humidity, speed);
 
-            // var card = $("<div>").addClass("card");
-            // var cardBody = $("<div>").addClass("card-body");
-            // var cardTitle = $("<h5>").addClass("card-title");
-            // var cardIcon = $("<img>").addClass("card-icon");
-            // var cardTemp = $("<p>").addClass("card-text");
-            // var cardWind = $("<p>").addClass("card-text");
-            // var cardHumidity = $("<p>").addClass("card-text");
-
-            // var date = new Date(dt * 1000);
-            // var month = date.getMonth()+1;
-            // var day = date.getDate();
-            // var year = date.getFullYear();
-            // var formattedDate = (month+"/"+day+"/"+year);
-
-            // var forecastIconUrl = "http://openweathermap.org/img/wn/" + id + ".png";
+            const forecastCard = forecast.addClass('card col-md-3');
             
-            // card.append(cardBody);
-            // cardBody.append(cardTitle.text(formattedDate));
-            // cardBody.append(cardIcon.attr('src', forecastIconUrl));
-            // cardBody.append(cardTemp.text('Temp: ' + temp + ' °F'));
-            // cardBody.append(cardHumidity.text('Wind: ' + speed + ' %'));
-            // cardBody.append(cardWind.text('Humidity: ' + humidity + ' %'));
+            forecastCard.html(`
+                <div class = "card-body bg-secondary text-white">
+                <h4 class = "card-title">${formattedDate}</h4>
+                <img src = "https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png"></img>
+                <p class = "card-text"> Temp: ${temp} °F</p>
+                <p class = "card-text"> Wind: ${speed} MPH</p>
+                <p class = "card-text"> Humidity: ${humidity} %</p>
+                </div>
+            `);
+            forecast.append(forecastCard);
         }
     },
     search: function() {
         this.fetchFutureWeather(document.querySelector(".input-search").value);
     }
-    }
-
-document.querySelector(".btn").addEventListener("click", function() {
-    event.preventDefault();
-    weather.search();
-    futureWeather.search();
-});
+}
