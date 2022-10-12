19826b183a538dc8b18e0fe378ea97000a728003
noah138 <noahschwartz@live.com>
2022-10-12 14:17:04 -0700

    Clicking on history correctly runsnow

--- assets/script.js
+++ assets/script.js
@@ -16,9 +16,8 @@ $(document).ready(() => {
 
     $('.list-group-item').on('click', (e) => {
         const city = e.target.textContent;
-        weather.fetchWeather(city);
-        weather.displayWeather(city);
-        getForecast();
+        weather.getWeather(city);
+        weather.getForecast(city);
     })
 })
 
@@ -26,10 +25,6 @@ clearHistory.on('click', function() {
     localStorage.clear();
     searchHistory.html("");
 });
-
-if (!localStorage.cities===null) {
-    hide
-}
 
 $('.search-btn').on("click", (e) => {
     event.preventDefault();
@@ -50,7 +45,7 @@ $('.search-btn').on("click", (e) => {
 })
 
 let weather = {
-    fetchWeather: function(city) {
+    getWeather: function(city) {
         fetch('https://api.openweathermap.org/data/2.5/weather?q='
         + city + '&appid=' + apiKey + '&units=imperial'
         )
@@ -71,58 +66,56 @@ let weather = {
         document.querySelector('#icon').src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
         document.querySelector('#city-temp').innerText = "Temp: " + temp + " °F"
         document.querySelector('#city-wind').innerText = "Wind: " + speed + " MPH"
-        document.querySelector('#city-humidity').innerText = "Humidity " + humidity + " %"
+        document.querySelector('#city-humidity').innerText = "Humidity: " + humidity + " %"
+    },
+    getForecast: function (city) {
+        let queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q='
+        + city + '&appid=' + apiKey + '&units=imperial'
+    
+        fetch(queryURL)
+        .then ((response) => {
+            return response.json();
+        })
+        .then((response) => {
+    
+            let forecastHTML = `
+            <h3>5-Day Forecast:</h3>
+            <div class="d-inline-flex flex-wrap justify-content-between">`;
+    
+            for (i=0; i<response.list.length; i++) {
+                let dt = response.list[i].dt;
+                let dateObject = new Date(dt*1000);
+                let formattedDate = dateObject.toLocaleDateString();
+                
+                let dayTimeUTC = response.list[i].dt;
+                let timeZoneOffset = response.city.timezone;
+                let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
+                let thisMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
+                let iconURL = "https://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png";
+        
+                if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
+                    forecastHTML += `
+                    <div class="card-body card col-1 col-md-2 m-1 text-center text-white bg-secondary">
+                        <ul class="list-unstyled p-1">
+                            <li>${formattedDate}</li>
+                            <li class="weather-icon"><img src="${iconURL}"></li>
+                            <li>Temp: ${response.list[i].main.temp} °F</li>
+                            <br>
+                            <li>Wind: ${response.list[i].wind.speed} MPH</li>
+                            <br>
+                            <li>Humidity: ${response.list[i].main.humidity}%</li>
+                        </ul>
+                    </div>`;
+                }
+            }
+    
+            forecastHTML += '</div>'
+    
+            $('.forecast').html(forecastHTML);
+        })
     },
     search: function() {
-        this.fetchWeather(document.querySelector(".input-search").value);
-        getForecast();
-    }
+        this.getWeather($('.input-search').val());
+        this.getForecast($('.input-search').val());
+    },
 };
-
-function getForecast () {
-    let city = $('.input-search').val();
-    let queryURL = 'https://api.openweathermap.org/data/2.5/forecast?q='
-    + city + '&appid=' + apiKey + '&units=imperial'
-
-    fetch(queryURL)
-    .then ((response) => {
-        return response.json();
-    })
-    .then((response) => {
-
-        let forecastHTML = `
-        <h3>5-Day Forecast:</h3>
-        <div class="d-inline-flex flex-wrap justify-content-between">`;
-
-        for (i=0; i<response.list.length; i++) {
-            let dt = response.list[i].dt;
-            let dateObject = new Date(dt*1000);
-            let formattedDate = dateObject.toLocaleDateString();
-            
-            let dayTimeUTC = response.list[i].dt;
-            let timeZoneOffset = response.city.timezone;
-            let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
-            let thisMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
-            let iconURL = "https://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png";
-    
-            if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
-                forecastHTML += `
-                <div class="card-body card col-2 p-1 m-2 text-center text-white bg-secondary">
-                    <ul class="list-unstyled p-1">
-                        <li>${formattedDate}</li>
-                        <li class="weather-icon"><img src="${iconURL}"></li>
-                        <li>Temp: ${response.list[i].main.temp} °F</li>
-                        <br>
-                        <li>Wind: ${response.list[i].wind.speed} MPH</li>
-                        <br>
-                        <li>Humidity: ${response.list[i].main.humidity}%</li>
-                    </ul>
-                </div>`;
-            }
-        }
-
-        forecastHTML += '</div>'
-
-        $('.forecast').html(forecastHTML);
-    })
-}
